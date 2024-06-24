7ec120aff82a76253b27c222900d4f807a4e0f57
Adrien Schneider <adrien.schneider-noury@netc.fr>
2024-06-24 02:41:10 +0200

    Working import Out to Not

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from notion_client import Client
 from O365 import Account, MSGraphProtocol
 from dotenv import dotenv_values
+import datetime as dt
+
 
 config = dotenv_values(".env")
 
@@ -19,12 +21,14 @@ calendar_dict = notion.databases.query(
 )
 
 clean_cal_list = []
+liste_events_noms = []
 for evenement in calendar_dict["results"]:
     temp_dict = {
         "ID": evenement["id"],
         "Name": evenement["icon"]["emoji"] + " | " + evenement["properties"]["Name"]["title"][0]["text"]["content"],
         "Type": evenement["properties"]["Type"]["select"]["name"]
     }
+    liste_events_noms.append(evenement["properties"]["Name"]["title"][0]["text"]["content"])
     date_dict = evenement["properties"]["Date"]["date"]
 
 
@@ -43,11 +47,11 @@ for evenement in calendar_dict["results"]:
     else:
         temp_dict["Date"] = date_format(date_dict, "start")
 
-    location_dict = evenement["properties"]["Location"]["select"]
-    if location_dict:
-        temp_dict["Location"] = location_dict["name"]
-    else:
-        temp_dict["Location"] = None
+    # location_dict = evenement["properties"]["Location"]["rich_text"]
+    # if location_dict:
+    #     temp_dict["Location"] = location_dict["name"]
+    # else:
+    #     temp_dict["Location"] = None
 
     clean_cal_list.append(temp_dict)
 
@@ -72,4 +76,24 @@ if account.authenticate(scopes=scopes):
     print('Authenticated!')
     schedule = account.schedule()
     calendar = schedule.get_default_calendar()
-    print(calendar.get_events())
+    query = calendar.new_query('start').greater_equal(dt.date.today())
+    events = calendar.get_events(query=query, include_recurring=False)
+    for event in events:
+        titre = event.subject[2:]
+        emoji = {
+            "type": "emoji",
+            "emoji": event.subject[0]
+        }
+        start = event.start.isoformat()
+        end = event.end.isoformat()
+        location = event.location['displayName']
+        if titre not in liste_events_noms:
+            properties = {
+                "Name": {"title": [{"text": {"content": titre}}]},
+                "Date": {"date": {"start": start}},
+                "Location": {"rich_text": [{"text": {"content": location}}]}
+            }
+            if end:
+                properties["Date"]["date"]["end"] = end
+
+            page = notion.pages.create(parent={"database_id": config["NOTION_DB_ID"]}, icon=emoji, properties=properties)
